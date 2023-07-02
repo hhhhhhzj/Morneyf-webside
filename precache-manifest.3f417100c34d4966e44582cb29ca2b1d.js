b73e89b0143ae3477e7f178f77ea2b8592b8bab8
unknown <2327475180@qq.com>
2023-07-02 16:05:28 +0800

    deploy

---
+++ precache-manifest.3f417100c34d4966e44582cb29ca2b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4b909e2a83842adcd4",
+    "url": "/Morneyf-webside/css/app.12cc8c15.css"
+  },
+  {
+    "revision": "c56936deec4584b8f4e901167458910b",
+    "url": "/Morneyf-webside/index.html"
+  },
+  {
+    "revision": "2c4b909e2a83842adcd4",
+    "url": "/Morneyf-webside/js/app.29ae30d7.js"
+  },
+  {
+    "revision": "c642f498684f045b4bc9",
+    "url": "/Morneyf-webside/js/chunk-vendors.c001ccb6.js"
+  },
+  {
+    "revision": "84d6002d07b8aeab528544cccb208068",
+    "url": "/Morneyf-webside/manifest.json"
+  },
+  {
+    "revision": "0d298544c7fa11fa31495d22da474697",
+    "url": "/Morneyf-webside/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Morneyf-webside/robots.txt"
+  }
+]);
